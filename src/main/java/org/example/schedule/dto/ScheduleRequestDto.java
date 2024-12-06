1291f5569954b6731ceb35c2f45f854bf831169e
dornrrl0932 <dornrrl0932@naver.com>
2024-12-06 21:31:27 +0900

    ScheduleRequestDto, ScheduleResponsDto 설계

--- src/main/java/org/example/schedule/dto/ScheduleRequestDto.java
+++ src/main/java/org/example/schedule/dto/ScheduleRequestDto.java
@@ -1,4 +1,15 @@
 package org.example.schedule.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class ScheduleRequestDto {
+
+    private Long id;
+    private String worktodo;
+    private String Authorname;
+    private Long password;
+
 }
